f4dc7ced6082abb4f953da27ccff0d25089b2bb4
tyleruploads <tyleruploads@yahoo.com>
2026-09-11 16:10:57 -0400

    Bump version number to v0.4.0+post and add version number to title screen

--- src/Scenes/title_screen.gd
+++ src/Scenes/title_screen.gd
@@ -1,9 +1,11 @@
 extends Node2D
+@onready var v_text_label = $Version
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var version: String = ProjectSettings.get_setting("application/config/version")
+	v_text_label.text = version
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
