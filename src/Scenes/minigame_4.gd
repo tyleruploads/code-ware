87f0bc79e5e3dd90567fa67833f95ebf33553e59
tyleruploads <tyleruploads@yahoo.com>
2026-09-16 15:45:54 -0400

    Add minigame 4: 'dodge the flying balls', issue before with code and image files not being included in commit

---
+++ src/Scenes/minigame_4.gd
@@ -0,0 +1,64 @@
+extends Node2D
+
+@export var minigame_name: String = "Dodge the Flying Balls!"
+@export var prompt: String = "DODGE!"
+@export var description: String = "Don't get hit by the balls! Move left and right with your arrow keys!"
+
+@onready var player: CharacterBody2D = $Player
+@onready var exampleMeteor = $Meteor
+@onready var timer: Label = $timer
+@onready var health_node: RichTextLabel = $health
+
+var health: float = 100.0
+var wait_to_spawn: float = 0.5
+
+func _process(delta: float) -> void:
+	# Handle spawning of meteors
+	if wait_to_spawn <= 0:
+		make_meteor()
+		wait_to_spawn = randf_range(0.6, 1.0)
+	else:
+		wait_to_spawn -= delta
+		
+	# Handle health
+	health_node.text = str(health) + "%"
+	
+	if health <= 0:
+		Global.minigames_done -= 1
+		Global.lives -= 1
+		var tree := Engine.get_main_loop() as SceneTree
+
+		if is_inside_tree() and get_tree():
+			get_tree().change_scene_to_file("res://Scenes/level_screen.tscn")
+			return
+		elif tree:
+			tree.change_scene_to_file("res://Scenes/level_screen.tscn")
+			return
+		else:
+			printerr("Could not access SceneTree to change scene!")
+			return
+
+
+func make_meteor():
+	var meteor = exampleMeteor.duplicate()
+	
+	# Get coordinates for meteor
+	var radius = meteor.get_node("CollisionShape2D").shape.radius
+	var obstacle_x = randf_range(0, get_viewport_rect().size.x - radius)
+	var obstacle_y = -400
+	
+	meteor.position = Vector2(obstacle_x, obstacle_y)
+	
+	get_parent().add_child(meteor)
+	meteor.show()
+	 
+	# Make it face the player
+	meteor.look_at(player.global_position)
+	
+	# Make meteor fly to player
+	var meteor_tween = meteor.create_tween()
+	
+	var time_till_hit = randfn(1, 2)
+	meteor_tween.tween_property(meteor, "global_position", player.global_position, time_till_hit)
+	
+	meteor_tween.tween_callback(meteor.queue_free)
---
+++ src/Scenes/minigame_4_timer.gd
@@ -0,0 +1,39 @@
+extends Label
+
+var time : float = 0.0
+
+# Called when the node enters the scene tree for the first time
+func _ready() -> void:
+	Global.minigames_done = 4
+	Timer(45.0)
+	pass
+
+# Called every frame
+# Delta is elapsed time since previous frame
+func _process(delta: float) -> void:
+	pass
+	
+func Timer(start_time: float) -> void:
+	time = start_time
+	
+	var tree := Engine.get_main_loop() as SceneTree
+	
+	while time > 0.001:
+		# Second argument automatically freezes when game is paused
+		await get_tree().create_timer(0.10, false).timeout
+		time -= 0.10
+		text = str(snapped(time, 0.1))
+		
+	time = 0.0
+	text = "0.0"
+	print("Timer finished")
+	
+	if is_inside_tree() and get_tree():
+		get_tree().change_scene_to_file("res://Scenes/level_screen.tscn")
+		return
+	elif tree:
+		tree.change_scene_to_file("res://Scenes/level_screen.tscn")
+		return
+	else:
+		printerr("Could not access SceneTree to change scene!")
+		return
