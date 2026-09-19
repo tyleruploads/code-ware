433bf8e53a247b66acbefb026f3e6c57957f9021
tyleruploads <tyleruploads@yahoo.com>
2026-09-19 12:35:12 -0400

    Add support for held jumps in Minigame 1: 'Orb Collection Platformer'

--- src/scripts/nonglobal/minigames/1/player.gd
+++ src/scripts/nonglobal/minigames/1/player.gd
@@ -9,7 +9,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("move_up") and is_on_floor():
+	if Input.is_action_pressed("move_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
